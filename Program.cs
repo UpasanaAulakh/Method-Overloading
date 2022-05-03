2cae43b6c7eae6e64ad42141c9ac0b0660ff6487
HP <HP@LAPTOP-D31FFQ0F>
2022-05-03 11:19:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    class Cal
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static float add(float a, float b)
+        {
+            return a + b;
+        } 
+         
+    public    static void Main(string[] args)
+        {
+            Console.WriteLine(Cal.add(23,98));
+            Console.WriteLine(Cal.add(27.4f, 9.65f));
+        }
+    }
+}
